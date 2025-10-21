217539b754e8c3d37f6a156767b6cdb6383a1061
Jbenasso <benassoj@email.sc.edu>
2025-10-21 13:29:17 -0400

    feat: initialize soundescape project with React and Vite
    - Added package.json with dependencies and scripts for development, build, and preview.
    - Created Layout component to structure the application with Header and Navigation.
    - Implemented Header component with basic styling.
    - Developed Navigation component for routing with links to Home and About pages.
    - Added CSS files for About, Header, Home, Navigation, and global styles.
    - Created Home and About pages with respective content and styling.
    - Configured Vite for React application.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'; 
+import "./../css/Header.css";
+
+const Header = () => {
+    return (
+        <div id="main-header">
+            <h1>Dogs</h1>
+        </div>
+    )
+};
+
+export default Header;
